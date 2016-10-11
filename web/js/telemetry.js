82a7293bce1c7f6e80ab02b98ff323d7f1bc74d0
Arsh Chauhan <achauhan@alaska.edu>
2016-10-11 14:46:04 -0800

    code formatting

--- web/js/telemetry.js
+++ web/js/telemetry.js
@@ -8,7 +8,9 @@ function robot_telemetry_t(div,robot,sim)
 {
     if (!div)
         return null;
+
     this.div = div;
+    
     if (!robot)
        { 
             this.power =
@@ -17,7 +19,7 @@ function robot_telemetry_t(div,robot,sim)
                 right:0.0,
                 mine:0.0,
                 dump:0.0,
-                roll:0.0,
+                roll:0.0
             };
         }
         
@@ -31,6 +33,7 @@ function robot_telemetry_t(div,robot,sim)
 
     if(sim)
         this.sim.power = sim.power;
+
     this.create_telemetry_gui();
 
     var _this = this;
@@ -45,9 +48,11 @@ robot_telemetry_t.prototype.create_telemetry_gui = function()
 {
     this.table = document.createElement('table');
     this.table.className= 'telemetry_table';
+    
     this.heading_row = document.createElement('tr');
     this.heading_row.appendChild(document.createElement('td')).appendChild(document.createElement('th')).appendChild(document.createTextNode("Sensor"));
     this.heading_row.appendChild(document.createElement('td')).appendChild(document.createElement('th')).appendChild(document.createTextNode("Value"));    
+    
     this.div.appendChild(this.table);
     this.table.appendChild(this.heading_row);
     
@@ -62,7 +67,7 @@ robot_telemetry_t.prototype.create_telemetry_gui = function()
 
     }
     var cells = document.getElementsByTagName('td');
-    for (var i=0;i<cells.length;++i)
+    for (var i=0;i<cells.length;++i) //Add class to all cells on page
     {
         cells[i].className = 'telemetry_cell';
     }
@@ -71,16 +76,16 @@ robot_telemetry_t.prototype.create_telemetry_gui = function()
 robot_telemetry_t.prototype.update_telemetry = function()
 {
     //FIX ME: Remove once real telemetry is available 
-    this.power.left = Math.random()*30;
-    this.power.right = Math.random()*60;
-    this.power.mine = Math.random()*90;
-    this.power.dump = Math.random();
-    this.power.roll = Math.random();
+    this.power.left = (Math.random()*30).toFixed(2);
+    this.power.right = (Math.random()*60).toFixed(2);
+    this.power.mine = (Math.random()*90).toFixed(2);
+    this.power.dump = (Math.random()).toFixed(2);
+    this.power.roll = (Math.random()).toFixed(2);
 
     var rows = document.getElementsByTagName('tr');
     
     for (var i=1; i<rows.length-1;++i) //Skip heading row 
     {
-        rows[i].children[1].childNodes[0].data=this.power.key(i-1).toFixed(2);
+        rows[i].children[1].childNodes[0].data=this.power.key(i-1);
     }
 }
